32ce678ea2d009b4ed8675e56eb1b76b6be91e73
Hoang Anh <htheanh2004@gmail.com>
2025-07-29 23:25:15 +0700

    feat(auth): implement jwt and ...

---
+++ src/modules/auth/passport/jwt.strategy.ts
@@ -0,0 +1,28 @@
+import { UsersService } from '@/modules/users/users.service';
+import { Injectable, UnauthorizedException } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { PassportStrategy } from '@nestjs/passport';
+import { ExtractJwt, Strategy } from 'passport-jwt';
+
+@Injectable()
+export class JwtStrategy extends PassportStrategy(Strategy) {
+  constructor(
+    private configService: ConfigService,
+    private usersService: UsersService,
+  ) {
+    super({
+      jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+      ignoreExpiration: false,
+      secretOrKey: configService.get<string>('JWT_ACCESS_TOKEN_SECRET') || '?',
+    });
+  }
+
+  async validate(payload: any) {
+    const user = await this.usersService.findOne(payload.sub);
+    if (!user) {
+      throw new UnauthorizedException();
+    }
+
+    return { _id: payload.sub, email: user.email, role: user.role };
+  }
+}
