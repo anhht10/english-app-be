f06944824905bee94a0d7eafbf271e0f37c377bc
Hoang Anh <htheanh2004@gmail.com>
2025-08-02 17:40:39 +0700

    test(auth): unit test for auth

--- src/modules/auth/auth.controller.spec.ts
+++ src/modules/auth/auth.controller.spec.ts
@@ -1,20 +1,149 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
+import { ConfigService } from '@nestjs/config';
+import { CreateAuthDto } from '@/modules/auth/dto/create-auth.dto';
+import { LoginAuthDto } from '@/modules/auth/dto/login.dto';
+import { UnauthorizedException } from '@nestjs/common';
 
 describe('AuthController', () => {
   let controller: AuthController;
+  let authService: AuthService;
+  const mockAuthService = {
+    register: jest.fn(),
+    login: jest.fn(),
+    refreshToken: jest.fn(),
+    logout: jest.fn(),
+    activateUser: jest.fn(),
+    forgotPassword: jest.fn(),
+    resetPasswordWithOtp: jest.fn(),
+    changePassword: jest.fn(),
+  };
+  const mockConfigService = {
+    get: jest.fn((key) => {
+      if (key === 'TTL_USER_CODE') return 30;
+      return null;
+    }),
+  };
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [AuthController],
-      providers: [AuthService],
+      providers: [
+        {
+          provide: AuthService,
+          useValue: mockAuthService,
+        },
+        {
+          provide: ConfigService,
+          useValue: mockConfigService,
+        },
+      ],
     }).compile();
 
     controller = module.get<AuthController>(AuthController);
+    authService = module.get<AuthService>(AuthService);
   });
 
   it('should be defined', () => {
     expect(controller).toBeDefined();
+  });
+
+  describe('register', () => {
+    it('should call authService.register', async () => {
+      const dto: CreateAuthDto = {
+        firstName: 'John',
+        lastName: 'Doe',
+        email: 'john.doe@example.com',
+        password: 'password',
+      };
+
+      mockAuthService.register.mockResolvedValue({
+        _id: '12345',
+      });
+
+      const result = await controller.register(dto);
+      expect(result).toEqual({ _id: '12345' });
+      expect(authService.register).toHaveBeenCalledWith(dto);
+    });
+  });
+
+  describe('login', () => {
+    it('should set cookie and return access token', async () => {
+      const dto: LoginAuthDto = {
+        email: 'john.doe@example.com',
+        password: 'password',
+      };
+
+      const req = { user: { id: '1' } };
+      const res = { cookie: jest.fn() };
+
+      mockAuthService.login.mockResolvedValue({
+        access_token: 'access_token',
+        refresh_token: 'refresh_token',
+      });
+
+      const result = await controller.login(dto, req, res);
+      expect(result).toEqual({ access_token: 'access_token' });
+      expect(authService.login).toHaveBeenCalledWith(req.user);
+    });
+  });
+
+  describe('refresh', () => {
+    it('should refresh token asnd set cookie', async () => {
+      const req = {
+        cookies: { refresh_token: 'refresh_token' },
+      };
+      const res = { cookie: jest.fn() };
+
+      mockAuthService.refreshToken.mockResolvedValue({
+        access_token: 'new_access_token',
+        refresh_token: 'new_refresh_token',
+      });
+
+      const result = await controller.refresh(req, res);
+      expect(result).toEqual({ access_token: 'new_access_token' });
+      expect(authService.refreshToken).toHaveBeenCalledWith(
+        req.cookies['refresh_token'],
+      );
+    });
+  });
+
+  describe('logout', () => {
+    it('should throw UnauthorizedException if no token is provided', async () => {
+      const req = {
+        headers: {
+          authorization: '',
+        },
+        cookies: {
+          refresh_token: 'refresh_token',
+        },
+      };
+
+      jest
+        .spyOn(authService, 'logout')
+        .mockRejectedValue(new UnauthorizedException('Invalid access token'));
+
+      await expect(authService.logout).rejects.toThrow(UnauthorizedException);
+    });
+
+    it('should call authService.logout with tokens', async () => {
+      const req = {
+        headers: {
+          authorization: 'Bearer access_token',
+        },
+        cookies: {
+          refresh_token: 'refresh_token',
+        },
+      };
+      mockAuthService.logout.mockResolvedValue(undefined);
+
+      const result = await controller.logout(req as any);
+      expect(result).toBeUndefined();
+      expect(authService.logout).toHaveBeenCalledWith(
+        req.headers.authorization.split(' ')[1],
+        req.cookies.refresh_token,
+      );
+    });
   });
 });
--- src/modules/auth/auth.service.spec.ts
+++ src/modules/auth/auth.service.spec.ts
@@ -1,12 +1,96 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { AuthService } from './auth.service';
+import { RedisService } from '@/core/redis/redis.service';
+import { ConfigService } from '@nestjs/config';
+import { JwtService } from '@nestjs/jwt';
+import { UsersService } from '@/modules/users/users.service';
+import { NotFoundException, UnauthorizedException } from '@nestjs/common';
 
 describe('AuthService', () => {
   let service: AuthService;
+  let usersService: Partial<UsersService>;
+  let jwtService: Partial<JwtService>;
+  let redisService: Partial<RedisService>;
+  let configService: Partial<ConfigService>;
+
+  const mockUser = {
+    _id: 'user-id',
+    email: 'user@example.com',
+    password: 'hashedpassword',
+    role: 'user',
+  };
+
+  const mockTokens: {
+    access_token: string;
+    refresh_token: `${string}-${string}-${string}-${string}-${string}`;
+  } = {
+    access_token: 'access-token',
+    refresh_token: '123e4567-e89b-12d3-a456-426614174000',
+  };
 
   beforeEach(async () => {
+    usersService = {
+      // async will automatically return a promise
+      // not really needed async but it like the real code
+      findByEmail: jest.fn().mockImplementation(async (email: string) => {
+        if (email.startsWith('non')) {
+          return null; // promise.resolve(null)
+        }
+        return mockUser;
+      }),
+      findOne: jest.fn().mockImplementation((_id: string) => {
+        if (_id.includes('non')) throw new NotFoundException();
+
+        return Promise.resolve({
+          ...mockUser,
+          _id: _id,
+        });
+      }),
+    };
+
+    jwtService = {
+      sign: jest.fn().mockReturnValue(mockTokens.access_token),
+      decode: jest.fn().mockReturnValue({
+        sub: mockUser._id,
+        email: mockUser.email,
+        role: mockUser.role,
+        jti: mockTokens.refresh_token,
+      }),
+    };
+
+    redisService = {
+      set: jest.fn(),
+      get: jest.fn(),
+      del: jest.fn(),
+    };
+
+    configService = {
+      get: jest.fn((key) => {
+        if (key === 'TTL_USER_CODE') return 30;
+        return null;
+      }),
+    };
+
     const module: TestingModule = await Test.createTestingModule({
-      providers: [AuthService],
+      providers: [
+        AuthService,
+        {
+          provide: UsersService,
+          useValue: usersService,
+        },
+        {
+          provide: JwtService,
+          useValue: jwtService,
+        },
+        {
+          provide: ConfigService,
+          useValue: configService,
+        },
+        {
+          provide: RedisService,
+          useValue: redisService,
+        },
+      ],
     }).compile();
 
     service = module.get<AuthService>(AuthService);
@@ -14,5 +98,127 @@ describe('AuthService', () => {
 
   it('should be defined', () => {
     expect(service).toBeDefined();
+  });
+
+  describe('validateUser', () => {
+    it('should validate user with correct credentials', async () => {
+      jest
+        .spyOn(require('@/common/helpers/util.ts'), 'comparePasswordHelper')
+        .mockResolvedValue(true);
+
+      const result = await service.validateUser(
+        mockUser.email,
+        mockUser.password,
+      );
+
+      console.log('validateUser result:', result);
+
+      expect(result).toEqual(mockUser);
+    });
+
+    it('should return null if user not found', async () => {
+      const result = await service.validateUser(
+        'non-existent@example.com',
+        'non-password',
+      );
+      expect(result).toBeNull();
+    });
+
+    it('shoutld return null if password is is invalid', async () => {
+      jest
+        .spyOn(require('@/common/helpers/util.ts'), 'comparePasswordHelper')
+        .mockResolvedValue(false);
+      const result = await service.validateUser(
+        mockUser.email,
+        'wrong-password',
+      );
+
+      expect(result).toBeNull();
+    });
+  });
+
+  describe('generateTokens', () => {
+    it('should generate access and refresh tokens', async () => {
+      // Pay attention here: because in generateTokens, randomUUID is called once,
+      // this function calls generateRefreshToken (error here) which also calls randomUUID
+      // so when we mock randomUUID, we need to ensure to mock for both calls
+      // instead using once mockReturnValueOnce, we can use mockReturnValue
+      // or we can do this
+      jest
+        .spyOn(crypto, 'randomUUID')
+        .mockReturnValueOnce('123e4567-e89b-12d3-a456-426614174000')
+        .mockReturnValueOnce(mockTokens.refresh_token); // for generateRefreshToken
+      redisService.set = jest.fn().mockResolvedValue(true);
+      jwtService.sign = jest.fn().mockReturnValue(mockTokens.access_token);
+
+      const tokens = await service.generateTokens(
+        mockUser._id,
+        mockUser.email,
+        mockUser.role,
+      );
+
+      expect(tokens).toEqual({
+        access_token: mockTokens.access_token,
+        refresh_token: mockTokens.refresh_token,
+      });
+    });
+  });
+
+  describe('generateRefreshToken', () => {
+    it('should throw UnauthorizedException if userId is not found in Redis', async () => {
+      redisService.get = jest.fn().mockResolvedValue(null);
+      const reslut = service.refreshToken('invalid-rt');
+      await expect(reslut).rejects.toThrow(UnauthorizedException);
+    });
+    it('should return new tokens if refresh token is valid', async () => {
+      redisService.get = jest.fn().mockResolvedValue(mockUser._id);
+      redisService.del = jest.fn().mockResolvedValue(true);
+      jest.spyOn(service, 'generateTokens').mockResolvedValue({
+        access_token: 'new-access-token',
+        refresh_token: '777e4567-e89b-12d3-a456-426614174000',
+      });
+      const result = await service.refreshToken(mockTokens.refresh_token);
+      expect(result).toEqual({
+        access_token: 'new-access-token',
+        refresh_token: '777e4567-e89b-12d3-a456-426614174000',
+      });
+    });
+
+    it('shoud throw UnauthorizedException if user not found', async () => {
+      redisService.get = jest.fn().mockResolvedValue('non-user-id');
+      await expect(service.refreshToken('non-existent-rt')).rejects.toThrow(
+        UnauthorizedException,
+      );
+    });
+  });
+
+  describe('login', () => {
+    it('should return tokens on successful login', async () => {
+      jest.spyOn(service, 'generateTokens').mockResolvedValue(mockTokens);
+      const result = await service.login(mockUser);
+      expect(result).toEqual(mockTokens);
+    });
+  });
+
+  describe('logout', () => {
+    it('should throw UnauthorizedException if refresh token is invalid', async () => {
+      jwtService.decode = jest.fn().mockReturnValue(null);
+      await expect(
+        service.logout('invalid-token', 'invalid-refresh-token'),
+      ).rejects.toThrow(UnauthorizedException);
+    });
+
+    it('it should delete the refresh token from Redis and add the access token to the blacklist', async () => {
+      jwtService.decode = jest.fn().mockReturnValue({
+        sub: mockUser._id,
+        jti: mockTokens.refresh_token,
+        email: mockUser.email,
+        role: mockUser.role,
+        exp: Date.now() + 1000 * 60 * 60,
+      });
+
+      redisService.del = jest.fn().mockResolvedValue(true);
+      redisService.set = jest.fn().mockResolvedValue(true);
+    });
   });
 });
